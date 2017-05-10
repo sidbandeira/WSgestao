92bcd95364d2c1556a40f74156a94248e348f8fc
sidnei <sid-bandeira@hotmail.com>
2017-05-09 22:33:51 -0300

    Atualizado persistencia do produto

--- PersistirDadosProduto.php
+++ PersistirDadosProduto.php
@@ -7,15 +7,15 @@
 </html>
 
 <?php
-//$servername = "mysql.hostinger.com.br";
-//$username = "u655756784_sid";
-//$password = "021082";
-//$dbname = "u655756784_temp";
+$servername = "mysql.hostinger.com.br";
+$username = "u655756784_sid";
+$password = "021082";
+$dbname = "u655756784_temp";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dbgestao";
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "dbgestao";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -34,16 +34,18 @@ foreach ($xml as $produto){
    $descricao = (string)$produto->produtodescricao;
    $precovenda = (float)$produto->produtoprecovenda;
    $precocusto = (float)$produto->produtoprecocusto;
+   $dtultimacompra = (String)$produto->produtodtultimacompra;
    $codintegracao = (int)$produto->codintegracao;
    $codempresa = (int)$produto->codempresa;
    $tiporegistro = (int) $produto->tiporegistro;
    
    if ($tiporegistro == 0) {
-       $sql = "INSERT INTO produto (produtodescricao, produtoprecovenda,produtoprecocusto,codintegracao, codempresa) "
-         . "VALUES ('$descricao', $precovenda, $precocusto, $codintegracao, $codempresa)";
+       $sql = "INSERT INTO produto (produtodescricao, produtoprecovenda,produtoprecocusto,produtodtultimacompra,codintegracao, codempresa) "
+         . "VALUES ('$descricao', $precovenda, $precocusto, '$dtultimacompra', $codintegracao, $codempresa)";
       
    }else{
-       $sql = "UPDATE produto SET produtodescricao = '$descricao', produtoprecovenda = $precovenda, produtoprecocusto = $precocusto"
+       $sql = "UPDATE produto SET produtodescricao = '$descricao', produtoprecovenda = $precovenda, produtoprecocusto = $precocusto,"
+               . " produtodtultimacompra = '$dtultimacompra'"
                . " WHERE codintegracao =  $codintegracao and codempresa =  $codempresa";
    }
     $conn->query($sql);
