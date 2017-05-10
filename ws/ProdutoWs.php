36dd53245ccf9b2239c4403f0b4b6b88f564bfd0
sidnei <sid-bandeira@hotmail.com>
2017-05-09 22:06:16 -0300

    Criado WS do produto

---
+++ ws/ProdutoWs.php
@@ -0,0 +1,49 @@
+<?php
+
+$servername = "mysql.hostinger.com.br";
+$username = "u655756784_sid";
+$password = "021082";
+$dbname = "u655756784_temp";
+
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "dbgestao";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+if (mysqli_connect_errno()) {
+    trigger_error(mysqli_connect_error());
+}
+$codEmpresa = htmlspecialchars($_GET["codempresa"]);
+
+//Consultando banco de dados
+$qryLista = mysqli_query($conn, "SELECT * FROM produto" 
+    ." INNER JOIN empresa USING(CodEmpresa)" 
+    ." INNER JOIN produtofornecedor USING(idproduto)"
+    ." INNER JOIN fornecedor USING(idfornecedor)"
+    ." WHERE empresa.codempresa = $codEmpresa");
+if ($qryLista->num_rows > 0) {
+
+    while ($row = $qryLista->fetch_assoc()) {
+        $data = $row["produtodtultimacompra"];
+        $old_date = Date_create($data);
+        $new_date = Date_format($old_date, "d/m/Y");
+        
+        $linha = array("idproduto" => $row["idproduto"], 
+                        "produtodescricao" => $row["produtodescricao"],
+                        "produtoprecovenda" => $row["produtoprecovenda"],
+                        "produtoprecocusto" => $row["produtoprecocusto"],
+                        "produtodtultimacompra" =>  $new_date
+            );
+        $produto['produtos'][] = $linha;
+    }
+} else {
+
+    $produto = 0;
+}
+
+//Passando vetor em forma de json
+echo json_encode($produto);
+?>
